d39ecc073ee7f07b9f41b4a1fc6c0217547036e4
Shashi Kant <130560334+1iamshashikant@users.noreply.github.com>
2023-09-27 16:27:55 +0530

    MakingDynamicElementinJSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,20 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expeseTitle = "Car Insurance";
+  const expenseAmount = "294.67";
+
+  const locationOfExpenditure = "Lucknow";
+
   return (
-    <div>
-      <div>
-        <h2>Expense Items</h2>
+    <div className="expense-item">
+      <div className="dates">{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2 className="items">{expeseTitle}</h2>
+        <div className="expense-item__location">{locationOfExpenditure}</div>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 200</p>
     </div>
   );
 }
